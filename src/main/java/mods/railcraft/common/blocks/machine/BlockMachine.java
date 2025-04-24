860b3c1cc4bf7302083f466c9795ef998429edad
Caedis <Caedis@users.noreply.github.com>
2025-04-24 13:02:47 -0500

    spotless

--- src/main/java/mods/railcraft/common/blocks/machine/BlockMachine.java
+++ src/main/java/mods/railcraft/common/blocks/machine/BlockMachine.java
@@ -14,7 +14,6 @@ import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.EnumCreatureType;
